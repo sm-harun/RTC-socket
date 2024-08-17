e0da1b39a5a7eea691ffba624c1d0faf3c338496
sm-harun <harunyuss12@gmail.com>
2024-08-17 16:35:35 +0300

    Initial repo

---
+++ socket.js
@@ -0,0 +1,16 @@
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
+});
+
+io.on("connection", socket => {
+  console.log(`User ${socket.id} connected`);
+
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id} disconnected`)
+  })
+  socket.on("new-message", (newMessage) => {
+    socket.broadcast.emit("recieve-message", newMessage);
+  })
+})
